d6905f32e0748dbe4421f054cd9dde2eef789ea0
ilra <ilari.raisanen@tuni.fi>
2020-06-24 15:49:48 +0300

    Didn't save before last push

--- index.js
+++ index.js
@@ -95,6 +95,7 @@ app.get("/info", (req, res) => {
   );
 });
 
-const port = 3001;
-app.listen(port);
-console.log(`Server running on port ${port}`);
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
