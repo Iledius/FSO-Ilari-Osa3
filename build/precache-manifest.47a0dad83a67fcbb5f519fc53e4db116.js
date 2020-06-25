5fd6e8e33b0bb3294d1e6cadb3fd15315d345674
ilra <ilari.raisanen@tuni.fi>
2020-06-25 12:04:41 +0300

    uibuild

---
+++ build/precache-manifest.47a0dad83a67fcbb5f519fc53e4db116.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0de55be915b627f04acdbc48d9e3d88e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3dda9424aa62515cadf3",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "214b12d6263b13e5c475",
+    "url": "/static/js/2.36a77bfc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.36a77bfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3dda9424aa62515cadf3",
+    "url": "/static/js/main.5b253bd9.chunk.js"
+  },
+  {
+    "revision": "24aecb121d5a1e31ca85",
+    "url": "/static/js/runtime-main.455ce434.js"
+  }
+]);
