3e9fa02336744a554a9b6a7326f36b2030335c66
ilra <ilari.raisanen@tuni.fi>
2020-07-10 13:45:36 +0300

    fixed errors with mongoose models

--- index.js
+++ index.js
@@ -26,6 +26,21 @@ const personSchema = new mongoose.Schema({
   number: String,
 });
 
+personSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+const person = new Person({
+  content: person_name,
+  number: person_number,
+});
+
 let persons = [
   {
     name: "Arto Hellas",
